d9528426a2cd380aefa5aa73c17fabec5a4ee316
Luke Orellana <orellanaluke@gmail.com>
2020-09-14 02:11:24 +0000

    Updated main.tf

--- redisserver/main.tf
+++ redisserver/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "lukelab-tf-state88"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
 
 provider "aws" {
   region = var.region
